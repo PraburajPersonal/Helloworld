6dcd9447cefa38ff3fe1d3c2e499f4193a5afda6
PraburajPersonal <praburaj@gmail.com>
2014-01-27 16:33:52 -0800

    Adding a comment

--- HelloWorld/HelloWorld/Startup.cs
+++ HelloWorld/HelloWorld/Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-            
+            //This displays a welcome page. 
             app.UseWelcomePage();
         }
     }
